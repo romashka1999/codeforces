34da90f9ff3560b66d1753c8a502ae9e7f20e7da
roma <roma.chikhladze777@gmail.com>
2020-07-31 13:45:06 +0400

    add 4A

---
+++ 4A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        if(x%2 == 0 && x > 2) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
